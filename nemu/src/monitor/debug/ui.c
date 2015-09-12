a5a9695cd980e2d5ead2e6202c2e1c38b0362173
tracer-ics2015 <tracer@njuics.org>
2015-09-12 02:01:07 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  02:01:07 up  4:27,  2 users,  load average: 0.02, 0.03, 0.05 e0b27b1a6965f55275472d07a7c522aebcee5e7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -73,33 +73,28 @@ static int cmd_info(char *args){
         return 0;     
 }
 
-static int cmd_x(char *args){
-        //char *tkp=strtok(args,"");
-        //printf("%s\n",tkp);   
-        char *s=strtok(NULL," ");;
-        //tkp=strtok(NULL,"");
-        //strcpy(s,tkp);
-        //tkp=strtok(NULL,"");
+static int cmd_x(char *args){   
+        char *s=strtok(NULL," ");
         
         int memory_number=atoi(s);    
 //get N
         
-        //printf("%s\n",tkp);
         char *expr=strtok(NULL," ");
-        //tkp=strtok(NULL,""); 
-        //strcpy(expr,tkp);
-        //tkp=strtok(NULL,"");
 //get expression
+
         swaddr_t addr;
         sscanf(expr,"%x",&addr);
         //printf("%s %x\n",expr,addr);
         int i;
         for(i=0;i<memory_number;i++){
-        //uint32_t memory_value=swaddr_read(addr,4);
-        printf("%x: %x\n",addr,swaddr_read(addr,4));
+        uint32_t memory_value=swaddr_read(addr,4);
+        printf("%x: %x\n",addr,memory_value);
         addr+=1;
-}
-                
+}       
+        /*unsigned long addr;
+        sscanf(expr,"%x",&addr);
+        uint32_t *ad=(uint32_t*)addr;
+        printf("%x\n",*ad);*/
         return 0;
 }
 
