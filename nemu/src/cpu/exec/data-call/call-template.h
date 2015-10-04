522bbcc2fe14b3ab791112720b708edbd7aa7716
tracer-ics2015 <tracer@njuics.org>
2015-10-04 01:41:37 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  01:41:37 up 21 min,  5 users,  load average: 0.00, 0.03, 0.04 40868637104e25766ebd916bac268a85979d555a

--- nemu/src/cpu/exec/data-call/call-template.h
+++ nemu/src/cpu/exec/data-call/call-template.h
@@ -10,7 +10,8 @@
 //make_instr_helper(rel32)
 
 make_helper(concat(call_rel32_, SUFFIX)){
-        cpu.eip+=instr_fetch(cpu.eip+1,4);
+        swaddr_t temp_addr=instr_fetch(cpu.eip+1,4);
+        cpu.eip=cpu.eip+temp_addr;
         print_asm_template1();  
         return 5; 
 }
