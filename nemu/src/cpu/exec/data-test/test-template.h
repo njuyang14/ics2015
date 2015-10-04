cb6b7eb36955981a95d0f5280d772149088b6e53
tracer-ics2015 <tracer@njuics.org>
2015-10-04 02:57:18 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  02:57:18 up  1:37,  5 users,  load average: 0.00, 0.01, 0.05 3c5ac9c9b4c7763bf1b1245b360db82752c18b9e

--- nemu/src/cpu/exec/data-test/test-template.h
+++ nemu/src/cpu/exec/data-test/test-template.h
@@ -3,7 +3,11 @@
 #define instr test
 
 static void do_execute() {
-        //OPERAND_W(op_dest,op_src->val);
+        //OPERAND_W(op_dest,op_src->val&op_dest->val);
+        swaddr_t temp=op_src->val&op_dest->val;
+        cpu.EFLAGS.ZF=(temp==0)?1:0;
+        cpu.EFLAGS.CF=0;
+        cpu.EFLAGS.OF=0; 
         print_asm_template2();
 }
 
