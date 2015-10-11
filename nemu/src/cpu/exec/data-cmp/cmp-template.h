ab5c2ab33223a47c1ab2feea50e99153dc3795d7
tracer-ics2015 <tracer@njuics.org>
2015-10-11 10:26:42 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  10:26:42 up  2:12,  5 users,  load average: 0.08, 0.03, 0.04 cd86e0fed8ef72742b6a0f314b3e6f30d4550f15

--- nemu/src/cpu/exec/data-cmp/cmp-template.h
+++ nemu/src/cpu/exec/data-cmp/cmp-template.h
@@ -5,9 +5,9 @@
 static void do_execute() {
         //OPERAND_W(op_dest, op_src->val);
         swaddr_t temp=op_dest->val-op_src->val;
-		int s_dest=op_dest->val>>31&1;
-		int s_src=op_src->val>>31&1;
-		int s_temp=temp>>31&1;
+		int s_dest=(op_dest->val>>31)&1;
+		int s_src=(op_src->val>>31)&1;
+		int s_temp=(temp>>31)&1;
 		int dest_and_src=!(s_dest^s_src);
 		int dest_and_temp=!(s_dest^s_temp);
 		cpu.EFLAGS.OF=dest_and_src|dest_and_temp;
