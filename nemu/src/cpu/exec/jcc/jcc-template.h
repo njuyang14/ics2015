62d4d53b54b7faeda8e381071f21b620f28b1e17
tracer-ics2015 <tracer@njuics.org>
2015-10-12 04:50:14 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  04:50:14 up 50 min,  2 users,  load average: 0.00, 0.01, 0.03 548d1d40b007e3d8f5b6bcfee079f097f4f406ba

--- nemu/src/cpu/exec/jcc/jcc-template.h
+++ nemu/src/cpu/exec/jcc/jcc-template.h
@@ -6,8 +6,9 @@ make_helper(concat(jbe_rel_,SUFFIX)){
 #define instr jbe
 	        if(cpu.EFLAGS.CF==1||cpu.EFLAGS.ZF==1){
 				swaddr_t temp_addr=instr_fetch(cpu.eip+1,DATA_BYTE);
-				cpu.eip=temp_addr&0xffff;
+				cpu.eip+=temp_addr;
             }
+			printf("eip=%x\n",cpu.eip);
 			print_asm_template1();
 			return 2;
 }
