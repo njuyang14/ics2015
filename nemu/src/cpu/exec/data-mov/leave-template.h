2dad3634c6f7e4fed89b4a393c4c670377e4bff0
tracer-ics2015 <tracer@njuics.org>
2015-10-14 02:54:18 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  02:54:18 up 33 min,  3 users,  load average: 0.16, 0.05, 0.05 48635ceba37e602d2d1e4eb2e897966310004e5e

--- nemu/src/cpu/exec/data-mov/leave-template.h
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -1,19 +1,19 @@
 #include "cpu/exec/template-start.h"
 
-#define instr leave
+//#define instr leave
 
-static void do_execute() {
-	        cpu.esp=cpu.ebp;//mov %ebp %esp
-			//OPERAND_W(cpu.ebp,MEM_R(cpu.esp));
-			MEM_W(cpu.ebp,MEM_R(cpu.esp));//pop ebp
-
-}
 
 make_helper(concat(leave_, SUFFIX)) {
-	        //swaddr_t addr = instr_fetch(eip + 1, 4);
-			//MEM_W(cpu.esp, cpu.ebp);
-			do_execute();
+	        if(DATA_BYTE==4){
+			cpu.esp=cpu.ebp;
+			MEM_W(cpu.ebp,MEM_R(cpu.esp));
 			cpu.esp=cpu.esp+4;
+			}
+			else{
+				 cpu.esp=cpu.gpr[5]._16;
+				 MEM_W(cpu.ebp,MEM_R(cpu.gpr[5]._16));
+				 cpu.esp=cpu.esp+2;
+			}
 			print_asm("leave\n");
 			return 1;
 }
