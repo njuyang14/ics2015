7a42b0416627e3607409fa05b32c336c0550b657
tracer-ics2015 <tracer@njuics.org>
2015-10-15 10:04:25 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  10:04:25 up  2:38,  4 users,  load average: 0.00, 0.02, 0.05 c105ec12922f0debecf74e015e50ec5733c61c7f

--- nemu/src/cpu/exec/data-je/je-template.h
+++ nemu/src/cpu/exec/data-je/je-template.h
@@ -14,11 +14,11 @@
 
 make_helper(concat(je_rel8_, SUFFIX)){
         if(cpu.EFLAGS.ZF==1){
-                swaddr_t temp_addr=instr_fetch(cpu.eip+1,DATA_BYTE);
+                int temp_addr=instr_fetch(cpu.eip+1,1);
 				printf("databyte=%d\n",DATA_BYTE);
 				printf("addr=%X\n",temp_addr);
-				//int shift=32-(DATA_BYTE<<3);
-				//temp_addr=(temp_addr<<shift)>>shift;
+				int shift=32-(DATA_BYTE<<3);
+				temp_addr=(temp_addr<<shift)>>shift;
 		        cpu.eip=cpu.eip+temp_addr;
         }
         print_asm("je %x",cpu.eip);
