50fbb370eb58184fc9b88f47891108a3affb58e5
tracer-ics2015 <tracer@njuics.org>
2015-10-25 09:44:10 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  09:44:10 up  2:19,  2 users,  load average: 0.00, 0.01, 0.05 289af28126f4789160546aeeba9c7bf31c10e4

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -87,7 +87,7 @@ void load_elf_tables(int argc, char *argv[]) {
 uint32_t print_var(char s[]){
 	int i,j;
 	int flag=1;
-	printf("strtab=%d\n",strtab[0]);
+	//printf("strtab=%d\n",strtab[0]);
 	for(i=0;i<nr_symtab_entry;i++){
 		for(j=0;j<strlen(s);j++){
 			if(strtab[symtab[i].st_name+j]!=s[j]){
@@ -95,13 +95,13 @@ uint32_t print_var(char s[]){
 				break;
 			}
 		}
+		if(flag==1&&j==strlen(s)){
+			return symtab[i].st_value;
+		}
 	}
-	if(flag==1)
-		return symtab[i].st_value;
-	else{
-		printf("The var is invalid.\n");
-		return 0;
-	}
+
+	printf("The var is invalid.\n");
+	return 0;
 }
 
 
