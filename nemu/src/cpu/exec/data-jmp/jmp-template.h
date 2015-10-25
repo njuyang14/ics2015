28dd33063cd2d5a8600a901b4b6239123e0dfd9f
tracer-ics2015 <tracer@njuics.org>
2015-10-25 03:01:58 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  03:01:58 up  1:08,  4 users,  load average: 0.00, 0.01, 0.05 a4c524f57e2bcf04e6a7340eb86d0ee646eaeabb

--- nemu/src/cpu/exec/data-jmp/jmp-template.h
+++ nemu/src/cpu/exec/data-jmp/jmp-template.h
@@ -6,24 +6,32 @@ make_helper(concat(jmp_rel_, SUFFIX)){
         swaddr_t temp_addr=instr_fetch(cpu.eip+1,DATA_BYTE);
         cpu.eip=cpu.eip+temp_addr;
 		if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
-        //printf("databute=%d\n",DATA_BYTE);
         print_asm("jmp %x",cpu.eip+DATA_BYTE+1);
         return DATA_BYTE+1;
 }
 
 make_helper(concat(jmp_rm_, SUFFIX)){
-		print_asm("implement\n");
-		return 2;
+	swaddr_t temp_addr=instr_fetch(cpu.eip+1,DATA_BYTE);
+	cpu.eip=temp_addr;
+	if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
+	print_asm("jmp %x",cpu.eip+DATA_BYTE+1);
+	return DATA_BYTE+1;
 }
 
 make_helper(concat(jmp_m16_, SUFFIX)){
-		print_asm("implement\n");
-		return 2;
+	swaddr_t temp_addr=instr_fetch(cpu.eip+1,DATA_BYTE);
+	cpu.eip=temp_addr;
+	if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
+	print_asm("jmp %x",cpu.eip+DATA_BYTE+1);
+	return DATA_BYTE+1;
 }
 
 make_helper(concat(jmp_ptr_, SUFFIX)){
-		print_asm("implement\n");
-		return 2;
+	swaddr_t temp_addr=instr_fetch(cpu.eip+1,DATA_BYTE);
+	cpu.eip=temp_addr;
+	if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
+	print_asm("jmp %x",cpu.eip+DATA_BYTE+1);
+	return DATA_BYTE+1;
 }
 
 #include "cpu/exec/template-end.h"
