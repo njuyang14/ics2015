7bbfbe36788a875f223a10267292c73bc585da42
tracer-ics2015 <tracer@njuics.org>
2015-10-28 11:55:31 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  11:55:31 up  1:31,  2 users,  load average: 0.11, 0.04, 0.05 4ad44bd020c8ce8120a1930d58c9f4b43f857fc9

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -109,16 +109,16 @@ uint32_t print_var(char *s){
 void print_stack_frame(){
 	int i=0,no=0;
 	uint32_t prebp=cpu.ebp;
+	//uint32_t ret_addr=swaddr_read(prebp+4,4);
 	while(prebp!=0){
 	  for(;i<nr_symtab_entry;i++){
-		  uint32_t ret_addr=swaddr_read(prebp+4,4);
 		  int func_addr=symtab[i].st_value+symtab[i].st_size;
-		  if(symtab[i].st_info==18&&cpu.eip<func_addr&&cpu.eip>symtab[i].st_value){
+		  uint32_t ret_addr=swaddr_read(prebp+4,4);
+		  if(symtab[i].st_info==18&&cpu.eip<=func_addr&&cpu.eip>=symtab[i].st_value){
 			  printf("#%d ",no);
 			  no++;
 
 			  printf("%x ",prebp);
-			
 			  int name_addr=symtab[i].st_name;
 			  int j=0;
 			  printf("in ");
@@ -131,14 +131,14 @@ void print_stack_frame(){
 			  int k=0;
 			  uint32_t temp=prebp;
 			  while(k<4){
-				  printf("%x ",swaddr_read(temp,4));
+				  printf("%x ",swaddr_read(temp+8,4));
 				  temp+=4;
 				  k++;
 			  }
 			  printf(")\n");
-			  prebp=swaddr_read(prebp,4);
+			  
 		  }
-		  else if(symtab[i].st_info==18&&ret_addr<func_addr&&ret_addr>symtab[i].st_value){
+		  else if(symtab[i].st_info==18&&ret_addr<=func_addr&&ret_addr>=symtab[i].st_value){
 		       printf("#%d ",no);
                no++;
 
@@ -153,10 +153,10 @@ void print_stack_frame(){
 		       }
 		       printf("(");
 
-               printf(")\n");	
-               prebp=swaddr_read(prebp,4);			   
+               printf(")\n");	 
 		  }
 	  }
+	  prebp=swaddr_read(prebp,4);
 	}
 }
 
