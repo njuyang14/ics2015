f10607449f49e1646be6bc96bb8bb0aaa7bbe5c3
tracer-ics2015 <tracer@njuics.org>
2015-10-31 08:43:55 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  08:43:55 up  1:17,  3 users,  load average: 0.00, 0.01, 0.03 9a629149b8a90e6a2e18b0dee6350b8dbab357

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,12 +3,17 @@
 #define instr movsx
 
 static void do_execute() {
-		DATA_TYPE addr=op_src->val;
-		int shift=32-(DATA_BYTE<<3);
-		addr=(addr<<shift)>>shift;
-
+		DATA_TYPE_S addr=op_src->val;
+		swaddr_t opcode=instr_fetch(cpu.eip+1,1);
+        if(opcode==0xbe){
+		addr=(addr<<24)>>24;
+		}
+		else if(opcode==0xbf){
+			addr=(addr<<16)>>16;
+	    }
 		OPERAND_W(op_dest, addr);
 		print_asm_template2();
-		}
-		make_instr_helper(rm2r)
- #include "cpu/exec/template-end.h"
+}
+
+make_instr_helper(rm2r)
+#include "cpu/exec/template-end.h"
