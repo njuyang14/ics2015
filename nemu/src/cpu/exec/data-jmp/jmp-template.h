4ec8efb3054d8031858096c40a629ba426a86be0
tracer-ics2015 <tracer@njuics.org>
2015-10-31 10:14:01 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  10:14:01 up  2:47,  3 users,  load average: 0.00, 0.01, 0.05 dc15529b36a837343ba1c94322ce6ee2a1aea56

--- nemu/src/cpu/exec/data-jmp/jmp-template.h
+++ nemu/src/cpu/exec/data-jmp/jmp-template.h
@@ -42,7 +42,7 @@ static void do_execute() {
 	}
 	else{
 		cpu.eip=addr;
-		if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
+		//if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
 	}
 	//print_asm("jmp %x",cpu.eip+DATA_BYTE+1);
 	print_asm_template1();
