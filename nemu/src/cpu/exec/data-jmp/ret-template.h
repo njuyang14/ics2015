e731c85127f7119448c2d08bf7da28a87f23d7b5
tracer-ics2015 <tracer@njuics.org>
2015-10-31 01:17:20 -0400

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  01:17:20 up 42 min,  3 users,  load average: 0.11, 0.04, 0.05 d2080bcf5fd1ff9a95b0957c9b49b10b0940fb

--- nemu/src/cpu/exec/data-jmp/ret-template.h
+++ nemu/src/cpu/exec/data-jmp/ret-template.h
@@ -3,8 +3,10 @@
 
 make_helper(concat(ret_, SUFFIX)) {
         cpu.eip=swaddr_read(cpu.esp,4);
-		//printf("eip=%x,databyte=%d\n",cpu.eip,DATA_BYTE);
-		cpu.esp=cpu.esp+DATA_BYTE;
+		if(DATA_BYTE==2)
+		    cpu.esp=cpu.esp+2;
+		else if(DATA_BYTE==4||DATA_BYTE==1)
+			cpu.esp=cpu.esp+4;
 		if(DATA_BYTE==2)cpu.eip=cpu.eip&0x0000FFFF;
 		print_asm("ret");
 		return 1;
