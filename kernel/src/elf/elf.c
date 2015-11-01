e6da3a2373bec591ab04b0d69f0e258dcd9c7c28
tracer-ics2015 <tracer@njuics.org>
2015-11-01 01:32:54 -0500

    >  run 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  01:32:54 up  3:08,  5 users,  load average: 0.00, 0.01, 0.02 9944b0e544b5b68743d5be80271dfcc67a914e4f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,22 +31,22 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	int ph=elf+elf->e_phoff;
-	int i=0;
+	ph=elf+elf->e_phoff;
+	int i;
 	//panic("please implement me");
-	for(;i<elf->e_phnum ;i++ ) {
+	for(i=0;i<elf->e_phnum ;i++ ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */	
-			 //ramdisk_read(, , ph->p_filesz);
+			 //ramdisk_read(,ph->p_offset, ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
