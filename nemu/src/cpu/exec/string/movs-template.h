0b19c72e42ee2e73f20dff91c4ad31f8b1c9ab2e
tracer-ics2015 <tracer@njuics.org>
2015-11-01 09:04:08 -0500

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  09:04:08 up 10:39,  3 users,  load average: 0.12, 0.05, 0.05 19d77642ca6fc14af6a0a135f3d5851e7622d5d

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,7 +3,37 @@
 #define instr movs
 
 make_helper(concat(movs_m_, SUFFIX)) {
-	cpu.edi=swaddr_read(cpu.esi,DATA_BYTE);
+	MEM_W(cpu.edi,swaddr_read(cpu.esi,DATA_BYTE));
+	if(DATA_BYTE==1){
+		if(cpu.EFLAGS.DF==0){
+		    cpu.edi++;
+		    cpu.esi++;
+		}
+		else{
+			cpu.edi--;
+			cpu.edi--;
+		}
+	}
+	else if(DATA_BYTE==2){
+		if(cpu.EFLAGS.DF==0){
+		    cpu.edi+=2;
+			cpu.esi+=2;
+		}
+		else{
+			cpu.edi-=2;
+			cpu.edi-=2;
+		}
+	}
+	else if(DATA_BYTE==2){
+	    if(cpu.EFLAGS.DF==0){
+			cpu.edi+=4;
+			cpu.esi+=4;
+	    }
+	    else{
+			cpu.edi-=4;
+			cpu.edi-=4;
+		}
+	}
 	print_asm("movs");
 	return 1;
 }
