17c9aca316dd1711d31396368efa545abcb12e2c
tracer-ics2015 <tracer@njuics.org>
2015-11-02 04:46:44 -0500

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  04:46:44 up 34 min,  4 users,  load average: 0.05, 0.03, 0.03 9a4fbcaad206ec53d7ea2697769adfd4104eb50

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -3,9 +3,28 @@
 #define instr stos
 
 make_helper(concat(stos_m_, SUFFIX)) {
-    //swaddr_t addr = instr_fetch(eip + 1, DATA_BYTE);
-	cpu.edi=REG(R_EAX);
-	print_asm("stos %%%s",REG_NAME(R_EAX));
-	return DATA_BYTE+1;
+	if(DATA_BYTE==1){
+		MEM_W(cpu.edi,cpu.gpr[0]._8[0]);
+		if(cpu.EFLAGS.DF==0)
+			cpu.edi+=1;
+		else
+			cpu.edi-=1;
+	}
+	else if(DATA_BYTE==2){
+		MEM_W(cpu.edi,cpu.gpr[0]._16);
+		if(cpu.EFLAGS.DF==0)
+			cpu.edi+=2;
+		else
+			cpu.edi-=2;
+	}
+	else if(DATA_BYTE==4){
+		MEM_W(cpu.edi,cpu.eax);
+		if(cpu.EFLAGS.DF==0)
+			cpu.edi+=4;
+		else
+			cpu.edi-=4;
+	}
+	print_asm_template1();
+	return DATA_BYTE;
 }
 #include "cpu/exec/template-end.h"
