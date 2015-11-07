0dab64bcf539cf5e16f33c129ffd963ab14aa518
tracer-ics2015 <tracer@njuics.org>
2015-11-07 04:33:19 -0500

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  04:33:19 up 45 min,  3 users,  load average: 0.09, 0.04, 0.05 6e71ee1a46956c0064fd7afcffd6de755696d0d

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -3,7 +3,81 @@
 #define instr setcc
 
 
-make_helper(concat(setcc_setne_,SUFFIX)){
+#if DATA_BYTE==1
+static void do_execute() {
+	DATA_TYPE opcode=instr_fetch(cpu.eip+1,1);
+	if(opcode==0x90){
+		if(cpu.EFLAGS.OF==1)op_src->val=1;
+		else op_src->val=0;
+	}
+	else if(opcode==0x91){
+		if(cpu.EFLAGS.OF==0)op_src->val=1;
+		else op_src->val=0;
+	}
+	else if(opcode==0x92){
+		if(cpu.EFLAGS.CF==1)op_src->val=1;
+		        else op_src->val=0;
+	}
+	else if(opcode==0x93){
+		if(cpu.EFLAGS.CF==0)op_src->val=1;
+		        else op_src->val=0;
+	}
+	else if(opcode==0x94){
+		if(cpu.EFLAGS.ZF==1)op_src->val=1;
+		        else op_src->val=0;
+	}
+	else if(opcode==0x95){
+		if(cpu.EFLAGS.ZF==0)op_src->val=1;
+		        else op_src->val=0;
+	}
+	else if(opcode==0x96){
+	if(cpu.EFLAGS.CF==1||cpu.EFLAGS.ZF==1)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x97){
+	if(cpu.EFLAGS.CF==0&&cpu.EFLAGS.ZF==0)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x98){
+	if(cpu.EFLAGS.SF==1)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x99){
+	if(cpu.EFLAGS.SF==0)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x9a){
+	if(cpu.EFLAGS.PF==1)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x9b){
+	if(cpu.EFLAGS.PF==0)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x9c){
+	if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x9d){
+	if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x9e){
+	if(cpu.EFLAGS.ZF==1||(cpu.EFLAGS.SF!=cpu.EFLAGS.OF))op_src->val=1;
+	        else op_src->val=0;
+	}
+	else if(opcode==0x9f){
+	if(cpu.EFLAGS.ZF==0&&(cpu.EFLAGS.SF==cpu.EFLAGS.OF))op_src->val=1;
+	        else op_src->val=0;
+	}
+	print_asm_template1();
+
+}
+
+make_instr_helper(rm)
+#endif
+
+/*make_helper(concat(setcc_setne_,SUFFIX)){
 	            if(cpu.EFLAGS.ZF==0){
 				cpu.gpr[0]._16=1;									
 				}
@@ -11,7 +85,6 @@ make_helper(concat(setcc_setne_,SUFFIX)){
 					cpu.gpr[0]._16=0; 
 				print_asm("setne %%al");
 				return 2;
-}
+}*/
 
 #include "cpu/exec/template-end.h"
-
