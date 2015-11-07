ba173861cb87f6bc91eb0b4818e3f1e93ff2ff40
tracer-ics2015 <tracer@njuics.org>
2015-11-07 04:44:01 -0500

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  04:44:01 up 56 min,  3 users,  load average: 0.04, 0.03, 0.05 b948145b58cd9d91a21fff5917704ed9713dfe40

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -7,68 +7,68 @@
 static void do_execute() {
 	DATA_TYPE opcode=instr_fetch(cpu.eip+1,1);
 	if(opcode==0x90){
-		if(cpu.EFLAGS.OF==1)op_src->val=1;
-		else op_src->val=0;
+		if(cpu.EFLAGS.OF==1)OPERAND_W(op_src,1);
+		else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x91){
-		if(cpu.EFLAGS.OF==0)op_src->val=1;
-		else op_src->val=0;
+		if(cpu.EFLAGS.OF==0)OPERAND_W(op_src,1);
+		else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x92){
-		if(cpu.EFLAGS.CF==1)op_src->val=1;
-		        else op_src->val=0;
+		if(cpu.EFLAGS.CF==1)OPERAND_W(op_src,1);
+		        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x93){
-		if(cpu.EFLAGS.CF==0)op_src->val=1;
-		        else op_src->val=0;
+		if(cpu.EFLAGS.CF==0)OPERAND_W(op_src,1);
+		        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x94){
-		if(cpu.EFLAGS.ZF==1)op_src->val=1;
-		        else op_src->val=0;
+		if(cpu.EFLAGS.ZF==1)OPERAND_W(op_src,1);
+		        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x95){
-		if(cpu.EFLAGS.ZF==0)op_src->val=1;
-		        else op_src->val=0;
+		if(cpu.EFLAGS.ZF==0)OPERAND_W(op_src,1);
+		        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x96){
-	if(cpu.EFLAGS.CF==1||cpu.EFLAGS.ZF==1)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.CF==1||cpu.EFLAGS.ZF==1)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x97){
-	if(cpu.EFLAGS.CF==0&&cpu.EFLAGS.ZF==0)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.CF==0&&cpu.EFLAGS.ZF==0)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x98){
-	if(cpu.EFLAGS.SF==1)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.SF==1)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x99){
-	if(cpu.EFLAGS.SF==0)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.SF==0)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x9a){
-	if(cpu.EFLAGS.PF==1)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.PF==1)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x9b){
-	if(cpu.EFLAGS.PF==0)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.PF==0)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x9c){
-	if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x9d){
-	if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x9e){
-	if(cpu.EFLAGS.ZF==1||(cpu.EFLAGS.SF!=cpu.EFLAGS.OF))op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.ZF==1||(cpu.EFLAGS.SF!=cpu.EFLAGS.OF))OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	else if(opcode==0x9f){
-	if(cpu.EFLAGS.ZF==0&&(cpu.EFLAGS.SF==cpu.EFLAGS.OF))op_src->val=1;
-	        else op_src->val=0;
+	if(cpu.EFLAGS.ZF==0&&(cpu.EFLAGS.SF==cpu.EFLAGS.OF))OPERAND_W(op_src,1);
+	        else OPERAND_W(op_src,0);
 	}
 	print_asm_template1();
 
