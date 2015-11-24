1b89e1a189ff49479671978d825186fa5024b661
tracer-ics2015 <tracer@njuics.org>
2015-11-24 10:19:12 -0500

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  10:19:12 up  1:08,  4 users,  load average: 0.01, 0.02, 0.05 1fd87791890172acf0b2f50771cb0f7af4b92e41

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -75,11 +75,12 @@ uint32_t read_cache1_hit(hwaddr_t addr,size_t len){
 			break;
 		}
 	}
-
+    
 	uint32_t data = L1[cache_no][block_no].offset[offset+len-1];
-	while(len-1>0){
-		data=(data<<8)+L1[cache_no][block_no].offset[offset+(--len)-1];
-	}
+	    while(len-1>0){
+		    data=(data<<8)+L1[cache_no][block_no].offset[offset+(--len)-1];
+	    }
+
 	return data;
 }
 
