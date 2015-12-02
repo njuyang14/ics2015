ef32b8b6c17ef7dd20a19b06d07320b8a66206d1
tracer-ics2015 <tracer@njuics.org>
2015-12-02 09:23:05 -0500

    >  compile NEMU 141220127 yx Linux debian 3.2.0-4-486 #1 Debian 3.2.68-1+deb7u3 i686 GNU/Linux  09:23:05 up  1:30,  4 users,  load average: 0.08, 0.03, 0.05 b51ced8317d2b7c4c4e2fb2977e2f567f17b8159

--- nemu/src/cpu/exec/lgdt/lgdt-template.h
+++ nemu/src/cpu/exec/lgdt/lgdt-template.h
@@ -4,17 +4,17 @@
 
 #if DATA_BYTE==2||DATA_BYTE==4
 make_helper(concat(lgdt_i_, SUFFIX)) {
-	uint16_t limit=instr_fetch(eip+1,1);
+	uint32_t addr=instr_fetch(eip+2,4);
 	uint32_t base;
 	if(DATA_BYTE==2){
-		base=instr_fetch(eip+2,2);
-		cpu.gdtr.limit=limit;
-		cpu.gdtr.base=base&0x00ffffff;
+		base=instr_fetch(instr_fetch(eip+2,2),4);
+		cpu.gdtr.limit=instr_fetch(addr,2);
+		cpu.gdtr.base=instr_fetch(addr+2,3)&0x00ffffff;
 	}
 	else{
 		base=instr_fetch(eip+2,4);
-		cpu.gdtr.limit=limit;
-		cpu.gdtr.base=base;
+		cpu.gdtr.limit=instr_fetch(addr,2);
+		cpu.gdtr.base=instr_fetch(addr+2,4);
 	}
     print_asm("lgdt 0x%x",base);
 	return 6;
